461cb2defcc3579fbe417954092ebfa7043e63c8
Pete Grady <notes4pete@gmail.com>
2023-11-15 13:53:14 -0600

    initial commit

---
+++ src/scripts/translation-literal.js
@@ -0,0 +1,255 @@
+export const translation = {
+	hype: {
+		en: 'Quality Work. Good Prices',
+		zh: '质量工作。好价格',
+		tradZh: '質量工作。好價格'
+	},
+	'company-copyright': {
+		en: '© Dream Home Renovation LLC',
+		zh: '© 梦想家居装修',
+		tradZh: '© 夢想家居裝修'
+	},
+
+	'company-name': {
+		en: 'Dream Home Renovation',
+		zh: '梦想家居装修',
+		tradZh: '夢想家居裝修'
+	},
+	'company-letters': {
+		en: 'DHR',
+		zh: '梦想家居装修',
+		tradZh: '夢想家居裝修'
+	},
+	'menu-home': {
+		en: 'Home',
+		zh: '主页',
+		tradZh: '主頁'
+	},
+	'menu-about': {
+		en: 'About',
+		zh: '关于',
+		tradZh: '關於'
+	},
+	'menu-gallery': {
+		en: 'Gallery',
+		zh: '画廊',
+		tradZh: '畫廊'
+	},
+	'menu-contact': {
+		en: 'Contact',
+		zh: '联系',
+		tradZh: '聯繫'
+	},
+	'menu-reviews': {
+		en: 'Reviews',
+		zh: '评论',
+		tradZh: '評論'
+	},
+	'hero-h1': {
+		en: 'Renovation Done, Right.',
+		zh: '装修有道',
+		tradZh: '装修有道'
+	},
+	'hero-subheading': {
+		en: 'High quality renovation that is done on time, and for a reasonable price - this is what you get with Dream Home Renovations.',
+		zh: '高质量的装修，按时完成，价格合理 – 这就是您在梦想家居装修中得到的',
+		tradZh: '高質量的裝修，按時完成且價格合理 – 這就是您在夢想家居裝修中得到的待遇。'
+	},
+	'hero-button': {
+		en: 'Free Consultation',
+		zh: '免费咨询',
+		tradZh: '免費諮詢'
+	},
+	'value-h1': {
+		en: 'We can transform your home into a stylish, comfortable and functional place.',
+		zh: '我们可以将您的家变成一个时尚、舒适和功能性强的地方',
+		tradZh: '我們可以將您的家變成一個時尚、舒適和功能性強的地方'
+	},
+	'value-subheading': {
+		en: "Dream Home Renovation will work with you to transform your home in any way you like. We will bring your vision to life, and improve your home's value. We strive to exceed your expectations.",
+		zh: ' 梦想家居装修将与您合作，根据您的喜好改造您的家。我们将实现您的愿景，提升您房屋的价值。我们努力超越您的期望。',
+		tradZh:
+			'夢想家居裝修能與您合作，根據您的喜好改造您的家。我們將實現您的願景，提升您房屋的價值。我們會努力超越您的期望。'
+	},
+	'business-value-h1': {
+		en: 'We can remodel your business',
+		zh: '我们也可以重新装修您的业务',
+		tradZh: '我們也可以重新裝修您的業務'
+	},
+	'business-value-subheading': {
+		en: 'Need that look to wow the customer? Talk to us! We have renovated many businesses from tea shoppes to salons.',
+		zh: '需要那种让客户惊叹的外观吗？和我们谈谈！我们已经装修了许多企业，从茶店到沙龙。',
+		tradZh: '需要那種讓客戶驚嘆的外觀嗎？和我們談談！我們已經裝修了許多企業，從茶店到沙龍。'
+	},
+	'process-h1': {
+		en: 'Our Process',
+		zh: '我们的流程',
+		tradZh: '我們的流程'
+	},
+	'process-subheading-1': {
+		en: 'Initial consultation to understand your needs',
+		zh: '初步咨询，了解您的需求',
+		tradZh: '初步諮詢，了解您的需求'
+	},
+	'process-subheading-2': {
+		en: 'Design suggestions and advice on how to achieve your vision',
+		zh: '设计建议，指导如何实现您的愿景',
+		tradZh: ' 設計建議，指導如何實現您的願景'
+	},
+	'process-subheading-3': {
+		en: 'Detailed quote with breakdown of costs',
+		zh: '详细的报价和成本分析',
+		tradZh: '詳細的報價和成本分析'
+	},
+	'process-subheading-4': {
+		en: 'After contract, work begins with careful supervision and quality control',
+		zh: '合同签订后，工作开始，进行仔细的监督和质量控制',
+		tradZh: '合同簽訂後，工作開始，進行仔細的監督和質量控制'
+	},
+	'process-subheading-5': {
+		en: 'Final inspection and walkthrough with customer(s) when the job is complete',
+		zh: '工作完成后，与客户进行最后的检查和演练',
+		tradZh: '工作完成後，與客戶進行最後的檢查和演練'
+	},
+	'about-h1': {
+		en: 'About Us',
+		zh: '关于我们',
+		tradZh: '關於我們'
+	},
+	'about-subheading': {
+		en: 'We are a young firm committed to providing excellent service at fair prices for our customers. No job is too small. Our construction manager has sixteen years experience in construction and can handle almost any job big or small.',
+		zh: '我们是一家年轻的公司，致力于为客户提供优质的服务和公平的价格。没有工作太小。我们的施工经理在建筑方面有十六年的经验，可以处理任何大小的工作。',
+		tradZh:
+			'我們是一家年輕的公司，致力於為客戶提供優質的服務和公平的價格。沒有工作太小。我們的施工經理在建築方面有十六年的經驗，可以處理任何大小的工作。'
+	},
+	'quality-h1': {
+		en: 'Commitment to Quality',
+		zh: '致力于质量',
+		tradZh: '致力於質量'
+	},
+	'quality-subheading': {
+		en: 'We are committed to using good quality materials, and excellent craftsmanship. We will make your dreams a reality. ',
+		zh: '我们致力于使用优质的材料和精湛的工艺。我们将使您的梦想成为现实。',
+		tradZh: '我們致力於使用優質的材料和精湛的工藝。我們將使您的夢想成為現實。'
+	},
+	'respect-h1': {
+		en: 'Respect for your Home and Business',
+		zh: '尊重您的家和企业',
+		tradZh: '尊重您的家和企業'
+	},
+	'respect-subheading': {
+		en: 'We strive to treat your home or business like it was our own. We don’t skimp on care just because we offer a good value. We take great care to clean up after ourselves.',
+		zh: '我们努力将您的家或企业视为自己的家。我们不会因为我们提供的价值而节省关心。我们非常注意打扫干净。',
+		tradZh:
+			'我們努力將您的家或企業視為自己的家。我們不會因為我們提供的價值而節省關心。我們非常注意打掃乾淨。'
+	},
+	'contact-h1': {
+		en: 'Contact Information',
+		zh: '联系信息',
+		tradZh: '聯繫信息'
+	},
+	'contact-email': {
+		en: 'Email: ',
+		zh: '电子邮件：',
+		tradZh: '電子郵件：'
+	},
+	'contact-phone': {
+		en: 'Phone: ',
+		zh: '电话：',
+		tradZh: '電話：'
+	},
+	'gallery-h1': {
+		en: 'Gallery',
+		zh: '画廊',
+		tradZh: '畫廊'
+	},
+	'gallery-subheading': {
+		en: 'Here are some examples of our work.',
+		zh: '这里有一些我们的工作示例',
+		tradZh: '這裡有一些我們的工作示例'
+	},
+	'reviews-h1': {
+		en: 'What Our Clients are Saying',
+		zh: '我们的客户在说什么',
+		tradZh: '我們的客戶在說什麼'
+	},
+	'reviews-subheading': {
+		en: 'Reviews from our recent clients',
+		zh: '我们最近客户的评论',
+		tradZh: '我們最近客戶的評論'
+	},
+	'review-name1': {
+		en: 'Christopher Cambell',
+		zh: 'Christopher Cambell',
+		tradZh: 'Christopher Cambell'
+	},
+	'review-text1': {
+		en: 'Dream Home Renovations did our garage floor in epoxy. They added race track tile and installed new age cabinets, added new lighting, and they even installed a lift. We now have the best garage in our development! Thanks.',
+		zh: '梦想家园装修在环氧树脂中做了我们的车库地板。他们添加了赛车瓷砖并安装了新时代的橱柜，增加了新的照明，他们甚至安装了升降机。我们现在有了我们发展中最好的车库！谢谢。',
+		tradZh:
+			'夢想家園裝修在環氧樹脂中做了我們的車庫地板。他們添加了賽車瓷磚並安裝了新時代的櫥櫃，增加了新的照明，他們甚至安裝了升降機。我們現在有了我們發展中最好的車庫！謝謝。'
+	},
+
+	'review-date1': {
+		en: 'Completed in May 2019',
+		zh: '2019年5月完成',
+		tradZh: '2019年5月完成'
+	},
+
+	'review-name2': {
+		en: 'Joshua Rawson',
+		zh: 'Joshua Rawson',
+		tradZh: 'Joshua Rawson'
+	},
+
+	'review-text2': {
+		en: 'Dream Home Renovations did a great job on our kitchen. They installed new cabinets, new granite countertops, new lighting and new appliances. They even installed a new tile floor. We love our new kitchen. Thanks.',
+		zh: '梦想家园装修在我们的厨房做了一份很好的工作。他们安装了新的橱柜，新的花岗岩台面，新的照明和新的电器。他们甚至安装了新的瓷砖地板。我们喜欢我们的新厨房。谢谢。',
+		tradZh:
+			'夢想家園裝修在我們的廚房做了一份很好的工作。他們安裝了新的櫥櫃，新的花崗岩臺面，新的照明和新的電器。他們甚至安裝了新的瓷磚地板。我們喜歡我們的新廚房。謝謝。'
+	},
+
+	'review-date2': {
+		en: 'Completed in June 2019',
+		zh: '2019年6月完成',
+		tradZh: '2019年6月完成'
+	},
+
+	'review-name3': {
+		en: 'MaryBeth Lee',
+		zh: 'MaryBeth Lee',
+		tradZh: 'MaryBeth Lee'
+	},
+
+	'review-text3': {
+		en: 'Dream Home Renovations helped spruce up our coffee shop. New floor, new counter tops and really cool lighting - Xiaoling helped us pick out the nicest furniture as well. Our shop is a hit.',
+		zh: '梦想家园装修帮助我们打扮我们的咖啡店。新地板，新柜台和非常酷的照明 - 小玲还帮助我们挑选了最好的家具。我们的商店很受欢迎。',
+		tradZh:
+			'夢想家園裝修幫助我們打扮我們的咖啡店。新地板，新櫃台和非常酷的照明 - 小玲還幫助我們挑選了最好的家具。我們的商店很受歡迎。'
+	},
+
+	'review-date3': {
+		en: 'Completed in July 2021',
+		zh: '2021年7月完成',
+		tradZh: '2021年7月完成'
+	},
+
+	'review-name4': {
+		en: 'Cindy Wu',
+		zh: 'Cindy Wu',
+		tradZh: 'Cindy Wu'
+	},
+
+	'review-text4': {
+		en: 'I wanted a whole house water filter with a pump for more water pressure installed. Dream Home Renovation did it at a great price, and now my water is so soft on my skin',
+		zh: '我想安装一个带有泵的整个房屋过滤器，以增加水压。梦想家园装修以很好的价格做到了这一点，现在我的水对我的皮肤很柔软',
+		tradZh:
+			'我想安裝一個帶有泵的整個房屋過濾器，以增加水壓。夢想家園裝修以很好的價格做到了這一點，現在我的水對我的皮膚很柔軟'
+	},
+
+	'review-date4': {
+		en: 'Completed in August 2021',
+		zh: '2021年8月完成',
+		tradZh: '2021年8月完成'
+	}
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        offwhite: "#FBF3E4",
+        plantgreen: "#7E7B34",
+        blandgray: "#F5F5F5",
+        boxgreen: "#DFEACE",
+        lightpink: "#F8ECEF",
+        gray6:"#f2f2f2"
+      },
+    },
+    plugins: [],
+  },
+};
